386d16b9b55347a8482248931af287645878bf81
Roman Chumachenko <chumachenko.roman@airslate.com>
2023-07-10 14:58:52 +0300

    Added log pulling on failures. Added sample failure.

--- app/src/main/java/com/example/githubtest/MainActivity.kt
+++ app/src/main/java/com/example/githubtest/MainActivity.kt
@@ -26,7 +26,7 @@ class MainActivity : AppCompatActivity() {
         val navController = findNavController(R.id.nav_host_fragment_content_main)
         appBarConfiguration = AppBarConfiguration(navController.graph)
         setupActionBarWithNavController(navController, appBarConfiguration)
-
+        asdf
         binding.fab.setOnClickListener { view ->
             Snackbar
                 .make(view, "Dismiss", Snackbar.LENGTH_LONG)
