3251087934ee671974337540f5c11468051eee7b
Roman Chumachenko <76726000+ChumachenkoRoman@users.noreply.github.com>
2023-07-20 10:31:49 +0300

    Bug fixes  (#8)

--- app/src/main/java/com/example/githubtest/SecondFragment.kt
+++ app/src/main/java/com/example/githubtest/SecondFragment.kt
@@ -15,8 +15,6 @@ class SecondFragment : Fragment() {
 
     private var _binding: FragmentSecondBinding? = null
 
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
 
     override fun onCreateView(
@@ -24,7 +22,6 @@ class SecondFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-
         _binding = FragmentSecondBinding.inflate(inflater, container, false)
         return binding.root
     }
@@ -35,6 +32,7 @@ class SecondFragment : Fragment() {
     ) {
         super.onViewCreated(view, savedInstanceState)
         binding.buttonSecond.setOnClickListener {
+
             findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
         }
     }
